d6a7a04613ca85c5cee325fb4fb09a69f9fe6200
Lisa <lisa.nierhaus@gmail.com>
2017-12-19 20:08:01 +0100

    ibc dashboard

--- src/Components/admin_account/accounts/Registrations.js
+++ src/Components/admin_account/accounts/Registrations.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import backendService from '../../../backendService';
-import image from '../../../../resources/ibc_logo.png'
+import image from '../../../../resources/ibc_logo.png';
+import done from '../../../../resources/done.png';
+import cancel from '../../../../resources/cancel.png'
 
 
 class Registrations extends Component {
@@ -32,6 +34,11 @@ class Registrations extends Component {
             .then(() => this.setState({
                 registeredAccounts: this.state.registeredAccounts.filter(account => account.id !== id)
             }));
+
+            const done = document.getElementById("done");
+
+            done.alert;
+
     };
 
     declineRegistration = (id) => {
@@ -70,9 +77,28 @@ class Registrations extends Component {
 
                   </div>
                 </nav>
-                <h1>Anfragen</h1>
+
+
+                <div className={"alert alert-success alert-dismissible fade show"} visibility={this.state.showButton ? "visible": "hidden"} role={"alert"} id={"done"}>
+                  Die Anfrage wurde erfolgreich akzeptiert!
+                    <button type="button" className={"close"} data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                </div>
+
+                <div className={"alert alert-danger alert-dismissible fade show"} role={"alert"}>
+                  Die Anfrage wurde erfolgreich abgelehnt!
+                  <button type="button" className={"close"} data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+
+                <div className={'headline'}>
+                    <h1>Anfragen</h1>
+                </div>
+
                 {this.state.registeredAccounts.length > 0 && (
-                    <table className={"table"}>
+                    <table className={"table table-striped"}>
                         <th>ID</th>
                         <th>Firmenname</th>
                         <th>Benutzername</th>
@@ -85,7 +111,20 @@ class Registrations extends Component {
                         {this.state.registeredAccounts.map((account) =>
                             <tr key={account.id}>
                                 <td>{account.id}</td><td>{account.company_name}</td><td>{account.name}</td><td>{account.email}</td><td>{account.website}</td><td>{account.company_type}</td><td>{account.status}</td>
-                                <td><button onClick={() => this.acceptRegistration(account.id)}>Accept</button><button onClick={() => this.declineRegistration(account.id)}>Decline</button></td>
+                                <td>
+                                    <div className={'float-left'}>
+                                        <div className={'done float-left'}>
+                                            <a onClick={() => this.acceptRegistration(account.id)}>
+                                                <img className={'done'} src={done} alt={'blub'}></img>
+                                            </a>
+                                        </div>
+                                        <div className={'cancel float-left'}>
+                                            <a onClick={() => this.declineRegistration(account.id)}>
+                                                <img className={'cancel'} src={cancel} alt={'blub'}></img>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </td>
                             </tr>
                         )}
                     </table>
