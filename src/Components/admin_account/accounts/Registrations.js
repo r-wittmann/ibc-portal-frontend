cc5293a33fcd6dc626ec1cc1d44a0e811982f6ce
Rainer Wittmann <rainer.wittmann@comsysto.com>
2018-01-08 10:05:24 +0100

    add notifications and extract RegistrationListItem

--- src/Components/admin_account/accounts/Registrations.js
+++ src/Components/admin_account/accounts/Registrations.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import backendService from '../../../backendService';
 import image from '../../../../resources/ibc_logo.png';
-
+import RegistrationListItem from './RegistrationListItem';
+import { toast } from "react-toastify";
 
 class Registrations extends Component {
     handleLogout = (event) => {
@@ -26,12 +27,9 @@ class Registrations extends Component {
         backendService.acceptAccount(id, companyType)
             .then(() => this.setState({
                 registeredAccounts: this.state.registeredAccounts.filter(account => account.id !== id)
-            }));
-
-        const done = document.getElementById("done");
-
-        done.removeAttribute('hidden');
-
+            }))
+            .then(() => toast('Rigistrierung akzeptiert', { type: 'success' }))
+            .catch(() => toast('Es ist ein Fehler aufgetreten', { type: 'error' }));
     };
 
     declineRegistration = (id) => {
@@ -40,11 +38,9 @@ class Registrations extends Component {
             .then(() => this.sendEmail(this.state.registeredAccounts.find(account => account.id === id)))
             .then(() => this.setState({
                 registeredAccounts: this.state.registeredAccounts.filter(account => account.id !== id)
-            }));
-
-        const cancel = document.getElementById("cancel");
-
-        cancel.removeAttribute('hidden');
+            }))
+            .then(() => toast('Registrierung abgelehnt', { type: 'success' }))
+            .catch(() => toast('Es ist ein Fehler aufgetreten', { type: 'error' }));
     };
 
     sendEmail = (account) => {
@@ -100,129 +96,40 @@ class Registrations extends Component {
 
                     </div>
                 </nav>
-
-
-                <div hidden className={"alert alert-success alert-dismissible fade show"} role={"alert"} id={"done"}>
-                    Die Anfrage wurde erfolgreich akzeptiert!
-                    <button type="button" className={"close"} data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-
-                <div hidden className={"alert alert-danger alert-dismissible fade show"} role={"alert"} id={"cancel"}>
-                    Die Anfrage wurde erfolgreich abgelehnt!
-                    <button type="button" className={"close"} data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-
                 <div className={'headline'}>
                     <h1>Anfragen</h1>
                 </div>
 
                 <div className={'container'}>
-                {this.state.registeredAccounts.length > 0 && (
-                    <table className={"table table-hover"}>
-                        <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Firmenname</th>
-                            <th>Benutzername</th>
-                            <th>E-Mail</th>
-                            <th>Webseite</th>
-                            <th>Firmentyp</th>
-                            <th>Status</th>
-                            <th>Aktionen</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {this.state.registeredAccounts.map((account) =>
-                            <tr key={account.id}>
-                                <td>{account.id}</td>
-                                <td>{account.company_name}</td>
-                                <td>{account.name}</td>
-                                <td>{account.email}</td>
-                                <td>{account.website}</td>
-                                <td>{account.company_type}</td>
-                                <td>{account.status}</td>
-                                <td>
-                                <div className={'btn-group'}>
-                                    <button className={'btn btn-outline-dark'}
-                                            data-toggle='modal'
-                                            data-target={'#'+account.id}>
-                                        <span className={'fa fa-check'}/>
-                                    </button>
-                                    <button className={'btn btn-outline-dark'}
-                                            onClick={() => this.declineRegistration(account.id)}>
-                                        <span className={'fa fa-times'}/>
-                                    </button>
-                                </div>
-                                    <div className="modal fade"
-                                         id={account.id}>
-                                        <div className="modal-dialog">
-                                            <div className="modal-content">
-                                                <div className="modal-body">
-                                                    Stimmt die Unternehmensart?
-                                                    <div className="form-check">
-                                                        <label className="form-check-label">
-                                                            <input className="form-check-input"
-                                                                   type="radio"
-                                                                   name="gridRadios"
-                                                                   value="ibc"
-                                                                   checked={account.company_type === 'ibc'}
-                                                                   onChange={(event) => this.handleChangeType(event, account.id)}/>
-                                                            IBC-Unternehmen
-                                                        </label>
-                                                    </div>
-                                                    <div className="form-check">
-                                                        <label className="form-check-label">
-                                                            <input className="form-check-input"
-                                                                   type="radio"
-                                                                   name="gridRadios"
-                                                                   value="startup"
-                                                                   checked={account.company_type === 'startup'}
-                                                                   onChange={(event) => this.handleChangeType(event, account.id)}/>
-                                                            Startup
-                                                        </label>
-                                                    </div>
-                                                    <div className="form-check">
-                                                        <label className="form-check-label">
-                                                            <input className="form-check-input"
-                                                                   type="radio"
-                                                                   name="gridRadios"
-                                                                   value="ngo"
-                                                                   checked={account.company_type === 'ngo'}
-                                                                   onChange={(event) => this.handleChangeType(event, account.id)}/>
-                                                            Gemeinnütziger Verein
-                                                        </label>
-                                                    </div>
-                                                </div>
-                                                <div className="modal-footer">
-                                                    <button type="button"
-                                                            onClick={() => {}}
-                                                            className="btn btn-secondary"
-                                                            data-dismiss="modal">Abbrechen
-                                                    </button>
-                                                    <button type="button"
-                                                            onClick={() => {
-                                                                this.acceptRegistration(account.id,
-                                                                    this.state.registeredAccounts.find(acc => acc.id === account.id).company_type);
-                                                            }}
-                                                            className="btn btn-primary"
-                                                            data-dismiss="modal">Speichern
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </td>
+                    {this.state.registeredAccounts.length > 0 && (
+                        <table className={"table table-hover"}>
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Firmenname</th>
+                                <th>Benutzername</th>
+                                <th>E-Mail</th>
+                                <th>Webseite</th>
+                                <th>Firmentyp</th>
+                                <th>Status</th>
+                                <th>Aktionen</th>
                             </tr>
-                        )}
-                        </tbody>
-                    </table>
-                )}
-
-                    </div>
+                            </thead>
+                            <tbody>
+                            {this.state.registeredAccounts.map((account) =>
+                                <RegistrationListItem
+                                    key={account.id}
+                                    account={account}
+                                    handleChangeType={this.handleChangeType}
+                                    handleAccept={this.acceptRegistration}
+                                    handleDecline={this.declineRegistration}
+                                    history={this.props.history}
+                                />
+                            )}
+                            </tbody>
+                        </table>
+                    )}
+                </div>
             </div>
         );
     }
