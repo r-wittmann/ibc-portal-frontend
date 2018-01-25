5411a9146974277f43441cc657b7701d48a2be7e
Rainer Wittmann <rainer.wittmann@comsysto.com>
2018-01-25 16:59:38 +0100

    public posting list item desing update

--- src/Components/student_account/postings/PublicPostingListItem.js
+++ src/Components/student_account/postings/PublicPostingListItem.js
@@ -4,21 +4,32 @@ import translate from "../../../translationService";
 
 class PublicPostingListItem extends Component {
 
-    
 
     render() {
         return (
 
-            <div className={"col-sm-4"}>
-                <div className={"card"} style={{marginBottom: 15}}>
-                { this.props.posting.logo ?
-                    <img className={"card-img-top"} src={this.props.posting.logo} alt={"Card image cap"}/> : <img/>
-                }
-                    <div className={"card-body"}>
-                        <h5 className={"card-title"}><Link to={`/postings/${this.props.posting.id}`}>{this.props.posting.title}</Link></h5>
-                        <h6 className={"card-subtitle mb-2 text-muted"}> <span className={'fa fa-map-marker'}/> {this.props.posting.place_of_employment} &nbsp; <span className={'fa fa-calendar-alt'}/> {translate.startOfEmployment(this.props.posting.start_of_employment)} </h6>
-                        <p className={"card-text"}> 
-                            <a href={`/postings/${this.props.posting.id}`} className={"btn btn-primary"}>Mehr Details</a>
+            <div className={'col-12 col-sm-10 col-md-6 col-lg-4 col-xl-3 offset-0 offset-sm-1 offset-md-0'}>
+                <div className={'card text-center'} style={{ height: 320, marginBottom: 15 }}>
+                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: 128 }}>
+                        {this.props.posting.logo &&
+                        <img src={this.props.posting.logo}
+                             style={{ maxWidth: 200, maxHeight: 100 }}
+                             alt={"Card image cap"}/>
+                        }
+                    </div>
+                    <div className={"card-body"} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
+                        <h5 className={"card-title"}>
+                            <Link to={`/postings/${this.props.posting.id}`}>
+                                {this.props.posting.title}
+                            </Link>
+                        </h5>
+                        <h6 className={"card-subtitle mb-2 text-muted"}><span
+                            className={'fa fa-map-marker'}/> {this.props.posting.place_of_employment} &nbsp; <span
+                            className={'fa fa-calendar-alt'}/> {translate.startOfEmployment(this.props.posting.start_of_employment)}
+                        </h6>
+                        <p className={"card-text"}>
+                            <a href={`/postings/${this.props.posting.id}`} className={"btn btn-primary"}>Mehr
+                                Details</a>
                         </p>
                     </div>
                 </div>
