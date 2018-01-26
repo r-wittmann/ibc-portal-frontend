81a2964792fb46fa8049764cc42e7f4a7a2d9137
Rainer Wittmann <rainer.wittmann@comsysto.com>
2018-01-26 13:12:23 +0100

    redesign student-view company profile

--- src/Components/student_account/company/PublicCompany.js
+++ src/Components/student_account/company/PublicCompany.js
@@ -24,68 +24,102 @@ class PublicCompany extends Component {
         return (
             <div>
                 <Header history={this.props.history}/>
-                <div className={'container'}>
-                    {this.state.loading
-                        ? <div className={'loader'}/>
-                        : (
-                            <div>
-                                <table className={'table table-borderless'}>
-                                    <tbody>
-                                    <tr>
-                                        <td>
-                                            <div className={'preview-headline'}>
-                                                <h2>{this.state.company.company_name.toUpperCase()}</h2>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div className={'preview-logo'}>
-                                                {this.state.company.logo &&
-                                                <img src={this.state.company.logo} alt={'logo'}/>
-                                                }</div>
-                                        </td>
-                                    </tr>
-                                    </tbody>
-                                </table>
+                <div className={'student-background'}>
+                    <div className={'container mt-0 mt-sm-4 mb-0 mb-sm-4 p-5'} style={{ backgroundColor: 'white' }}>
+                        {this.state.loading
+                            ? <div className={'loader'}/>
+                            : (
+                                <div>
+                                    <div className={'row'}>
+                                        <div className={'d-none d-sm-none d-md-block col-md-8 my-auto'}>
+                                            <h2>{this.state.company.company_name.toUpperCase()}</h2>
+                                        </div>
+                                        <div className={'col-4'}>
+                                            {this.state.company.logo &&
+                                            <img src={this.state.company.logo} alt={'logo'} className={'img-responsive'}
+                                                 style={{ maxHeight: 100, maxWidth: 200 }}/>
+                                            }
+                                        </div>
+                                        <div className={'d-block d-sm-block d-md-none col-sm-12 my-auto'}>
+                                            <h2 style={{ fontSize: '120%' }}>{this.state.company.company_name.toUpperCase()}</h2>
+                                        </div>
+                                    </div>
 
-                                <div className={'attributes'}>
-                                    <table className={'table table-borderless preview-table'}>
-                                        <tbody>
-                                        <tr>
-                                            <td><span className={'fa fa-info-circle'}/> <a
-                                                href={this.state.company.website}> {this.state.company.website}</a></td>
-                                            <td><span
-                                                className={'fa fa-users'}/> {translate.numberOfEmployees(this.state.company.employees)} Mitarbeiter
-                                            </td>
-                                            <td><span className={'fa fa-map-marker'}/> <span
-                                                dangerouslySetInnerHTML={{ __html: this.state.company.munich_address.split('\n').join('<br>') }}/>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            {this.state.company.field_of_activity ?
-                                                <td><span className={'fa fa-search'}/> {this.state.company.field_of_activity}
-                                                </td> : <td></td>
+                                    <div className={'row'}>
+                                        <div className={'col-12 my-5'}
+                                             style={{
+                                                 height: 1,
+                                                 backgroundColor: '#9a9a9a'
+                                             }}/>
+                                    </div>
+                                    <div className={'row'}>
+                                        <div className={'col-sm-12 col-md-5 col-lg-4 order-1 order-sm-1 order-md-2'}>
+                                            <div className={'row mb-2'}>
+                                                <div className={'col-2 text-center'}>
+                                                    <span className={'fa fa-info-circle'}/>
+                                                </div>
+                                                <div className={'col-10'}>
+                                                    <a href={this.state.company.website}>{this.state.company.website.split('//')[1]}</a>
+                                                </div>
+                                            </div>
+                                            <div className={'row mb-2'}>
+                                                <div className={'col-2 text-center'}>
+                                                    <span className={'fa fa-users'}/>
+                                                </div>
+                                                <div className={'col-9'}>
+                                                    {translate.numberOfEmployees(this.state.company.employees)} Mitarbeiter
+                                                </div>
+                                            </div>
+                                            <div className={'row mb-2'}>
+                                                <div className={'col-2 text-center'}>
+                                                    <span className={'fa fa-search'}/>
+                                                </div>
+                                                <div className={'col-9'}>
+                                                    Haupttätigkeitsbereich: {this.state.company.field_of_activity}
+                                                </div>
+                                            </div>
+                                            {this.state.company.kununu &&
+                                            <div className={'row mb-2'}>
+                                                <div className={'col-2 text-center'}>
+                                                    <span className={'fa fa-star'}/>
+                                                </div>
+                                                <div className={'col-9'}>
+                                                    <a href={this.state.company.kununu}>Bewertung auf kununu</a>
+                                                </div>
+                                            </div>
                                             }
-                                            {this.state.company.kununu ?
-                                                <td><span className={'fa fa-star'}/> <a
-                                                    href={this.state.company.kununu}> Bewertungen auf kununu</a></td> :
-                                                <td></td>
-                                            }
-                                            {this.state.company.locations ?
-                                                <td><span className={'fa fa-globe'}/> {this.state.company.locations}
-                                                </td> : <td></td>
-                                            }
-                                        </tr>
-                                        </tbody>
-                                    </table>
+                                            <div className={'row mb-2'}>
+                                                <div className={'col-2 text-center'}>
+                                                    <span className={'fa fa-globe'}/>
+                                                </div>
+                                                <div className={'col-9'}>
+                                                    {this.state.company.locations}
+                                                </div>
+                                            </div>
+                                            <div className={'row mb-2'}>
+                                                <div className={'col-2 text-center'}>
+                                                    <span className={'fa fa-map-marker'}/>
+                                                </div>
+                                                <div className={'col-9'}>
+                                                <span
+                                                    dangerouslySetInnerHTML={{ __html: this.state.company.munich_address.replace('\n', '<br>') }}/>
+                                                </div>
+                                            </div>
+                                            <div className={'row'}>
+                                                <div
+                                                    className={'col-12 d-block d-sm-block d-md-none d-lg-none d-xl-none my-5'}
+                                                    style={{
+                                                        height: 1,
+                                                        backgroundColor: '#9a9a9a'
+                                                    }}/>
+                                            </div>
+                                        </div>
+                                        <div className={'col-sm-12 col-md-7 col-lg-8 order-2 order-sm-2 order-md-1'}>
+                                            <p dangerouslySetInnerHTML={{ __html: draftToHtml(JSON.parse(this.state.company.company_description)) }}/>
+                                        </div>
+                                    </div>
                                 </div>
-                                <p dangerouslySetInnerHTML={{ __html: draftToHtml(JSON.parse(this.state.company.company_description)) }}/>
-                            </div>
-                        )}
-                    <div className='float-right'>
-                        <button className={'btn btn-primary'}
-                                onClick={() => this.props.history.goBack()}>
-                            Zurück
-                        </button>
+                            )}
                     </div>
                 </div>
             </div>
